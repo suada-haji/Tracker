f23f3d948f7c8d1af5fc57a0bf8499cfd68d9842
Suada Haji <suada.haji@andela.com>
2016-05-16 22:44:46 +0300

    Create a Location Intent Service and add it to the Manifest

--- app/src/main/java/com/checkpoint/andela/mytracker/helpers/Constants.java
+++ app/src/main/java/com/checkpoint/andela/mytracker/helpers/Constants.java
@@ -5,6 +5,20 @@ package com.checkpoint.andela.mytracker.helpers;
  */
 public class Constants {
 
+    public static final String PACKAGE_NAME = "com.checkpoint.andela.mytracker";
+
+    public static final String STRING_ACTION = PACKAGE_NAME + ".STRING_ACTION";
+
+    public static final String STRING_EXTRA = PACKAGE_NAME + ".STRING_EXTRA";
+
+    public static final int SUCCESS_RESULT = 0;
+    public static final int FAILURE_RESULT = 1;
+
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
+    public static final String RESULT_ADDRESS = PACKAGE_NAME + ".RESULT_ADDRESS";
+    public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_DATA_EXTRA";
+
     public static final String TIME_KEY = "TIME_KEY";
     public static final String DEFAULT_DELAY_TIME = "5 minutes";
     public static final String MINUTES = " min";
