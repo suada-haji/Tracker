a36cd47ace7adb2850c38345e93eb81879e1bc4c
Suada Haji <suada.haji@andela.com>
2016-05-23 10:18:20 +0300

    Change the background color of the Linear Layout #119619717

--- app/src/main/java/com/checkpoint/andela/mytracker/activities/ListActivity.java
+++ app/src/main/java/com/checkpoint/andela/mytracker/activities/ListActivity.java
@@ -1,12 +1,9 @@
 package com.checkpoint.andela.mytracker.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.checkpoint.andela.mytracker.R;
 import com.checkpoint.andela.mytracker.helpers.ActivityLauncher;
